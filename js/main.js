abded8fec2abf14a0a2d111e15084ab3fa9233a9
Margarita Dainitsyna <mdaynicyna@gmail.com>
2022-10-30 00:33:00 +0700

    fix

--- js/main.js
+++ js/main.js
@@ -1,14 +1,68 @@
 window.addEventListener('DOMContentLoaded', () => {
+  showMorePodcasts()
   showGuestInfo()
   openMenu()
+  openAir()
+  openLogin()
+  validateForm()
+  swiper()
+  choices()
+  accordion()
+
 })
 
-function openMenu () {
-  document.querySelector('#burger').addEventListener('click', () => {
-      document.querySelector('#menu').classList.add('is-open')
+function openLogin() {
+  document.querySelector('#login').addEventListener('click', () => {
+    document.querySelector('#modal').style.display = 'block'
   })
-  document.querySelector('#close').addEventListener('click',function() {
-      document.querySelector('#menu').classList.remove('is-open')
+  document.querySelector('#closeModal').addEventListener('click', () => {
+    document.querySelector('#modal').style.display = 'none'
+  })
+}
+
+function showMorePodcasts() {
+  let podcastBtn = document.querySelector('#podcastsBtn');
+
+  podcastBtn.addEventListener('click', () => {
+    if (podcastBtn.innerText === "Ещё подкасты") {
+      for (pod of document.querySelectorAll('.podcasts__item')) {
+        pod.style.display = 'flex'
+      }
+      podcastBtn.innerText = "Скрыть"
+    } else if (podcastBtn.innerText === "Скрыть") {
+
+      for (i = 4; i <= (document.querySelectorAll('.podcasts__item').length); i++) {
+        document.querySelectorAll('.podcasts__item')[i].style.display = 'none'
+        podcastBtn.innerText = "Ещё подкасты"
+      }
+    }
+  })
+}
+
+function openMenu() {
+  if (document.documentElement.clientWidth <= 500) {
+    document.querySelector('#burger').addEventListener('click', () => {
+      document.querySelector('#menu').classList.add('menu-open')
+      document.querySelector('#topics').classList.add('topics-open')
+    })
+    document.querySelector('#close').addEventListener('click', function () {
+      document.querySelector('#menu').classList.remove('menu-open')
+      document.querySelector('#topics').classList.remove('topics-open')
+    })
+  } else {
+    document.querySelector('#burger').addEventListener('click', () => {
+      document.querySelector('#menu').classList.add('menu-open')
+    })
+    document.querySelector('#close').addEventListener('click', function () {
+      document.querySelector('#menu').classList.remove('menu-open')
+    })
+  }
+}
+
+function openAir() {
+  document.querySelector('#air').addEventListener('click', () => {
+    document.querySelector('.air__player').classList.toggle('is-show')
+    document.querySelector('.air__open').classList.toggle('air-opened')
   })
 }
 
@@ -17,9 +71,9 @@ function showGuestInfo() {
     item.addEventListener('click', () => {
 
       if (document.querySelector('.chosen')) {
-        document.querySelector('.chosen').classList.remove('chosen');
+        document.querySelector('.chosen').classList.remove('chosen')
       }
-      item.classList.add('chosen');
+      item.classList.add('chosen')
 
       if (item.hasAttribute('data-descr')) {
         document.querySelectorAll('.link-disabled').forEach((link) => {
@@ -27,8 +81,8 @@ function showGuestInfo() {
         })
 
         document.querySelector('.guest__img').style.backgroundImage = `url('../img/${item.dataset.img}')`
-        document.querySelector('.guest__name').textContent = item.innerText;
-        document.querySelector('.guest__description').textContent = item.dataset.descr;
+        document.querySelector('.guest__name').textContent = item.innerText
+        document.querySelector('.guest__description').textContent = item.dataset.descr
 
         let socialLinks = document.querySelector('.guest__social').querySelectorAll('.social__link');
         socialLinks[0].setAttribute("href", item.dataset.links.split(', ')[0])
@@ -39,4 +93,111 @@ function showGuestInfo() {
       }
     })
   })
+}
+
+function choices() {
+  const element = document.querySelector('#select');
+  const choices = new Choices(element, {
+    searchEnabled: false,
+    itemSelectText: '',
+  });
+}
+
+function accordion() {
+  new Accordion('.accordion-container')
+}
+
+function swiper() {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    freeMode: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+      651: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1025: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      }
+    },
+  });
+}
+
+function validateForm() {
+  const validate = new window.JustValidate('#form', {
+    errorLabelStyle: {
+      color: 'var(--error)',
+    },
+    errorFieldStyle: {
+      border: '1px solid var(--error)',
+    },
+  });
+
+  validate
+    .addField('.input__massage', [{
+      rule: 'required',
+      errorMessage: 'Напишите сообщение',
+    }, ])
+    .addField('.input__name', [{
+        rule: 'minLength',
+        value: 3,
+        errorMessage: "Слишком короткое имя"
+      },
+      {
+        rule: 'maxLength',
+        value: 30,
+        errorMessage: "Слишком длинное имя"
+      },
+      {
+        rule: 'required',
+        errorMessage: 'Введите имя',
+      },
+    ])
+    .addField('.input__email', [{
+        rule: 'required',
+        errorMessage: 'Введите почту',
+      },
+      {
+        rule: 'email',
+        errorMessage: 'Неверно указана почта',
+      }
+    ])
+    .addField('#check', [{
+      rule: 'required',
+      errorMessage: 'Согласие обязательно',
+    }, ])
+
+  const validateLogin = new window.JustValidate('.modal__form');
+
+  validateLogin
+    .addField('.input__login', [{
+        rule: 'minLength',
+        value: 3,
+        errorMessage: "Слишком короткий логин"
+      },
+      {
+        rule: 'maxLength',
+        value: 30,
+        errorMessage: "Слишком длинный логин"
+      },
+      {
+        rule: 'required',
+        errorMessage: 'Введите логин',
+      },
+    ])
+    .addField('.input__pass', [{
+      rule: 'required',
+      errorMessage: 'Введите пароль',
+    }, {
+      rule: 'password',
+      errorMessage: 'пароль',
+    }, ])
+
 }
